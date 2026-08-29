61277a28718092ad797ee0f30924cf0bb031c11f
David Rios <yosoydrios@gmail.com>
2026-08-28 22:15:41 -0500

    proyecto agregado

--- test/test_nomina.py
+++ test/test_nomina.py
@@ -1,8 +1,8 @@
 import unittest
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
-import src.model.logica_nomina as logica_nomina
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from src.model import logica_nomina
 
 
 class TestsLiquidacionNomina(unittest.TestCase):
